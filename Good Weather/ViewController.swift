749ec708e89c2efc6550e35abdd578f25887695c
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-08-25 16:13:54 +0700

    init project

---
+++ Good Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Good Weather
+//
+//  Created by TheGIZzz on 25/8/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+
+
+}
+
